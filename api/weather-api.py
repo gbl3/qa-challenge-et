f44e18aefc685c3b785c8a7783ea5765a53fed25
Gustavo <gustavoberlopes@gmail.com>
2023-10-17 19:11:44 -0300

    improves weather api tests by reusing code

--- api/weather-api.py
+++ api/weather-api.py
@@ -2,139 +2,95 @@ import requests
 from datetime import datetime
 
 
-def test_valid_lat_lon_get_realtime_weather_api():
-    url: str = "https://weatherapi-com.p.rapidapi.com/current.json"
-
-    querystring: dict = {"q": "-8.063200,-34.871113"}
-
-    headers: dict = {
-        "X-RapidAPI-Key": 'e75696bd7cmsh2c6aeb979fa9e25p144730jsnf62c4bde84cd',
-        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
-    }
-
-    response = requests.get(url, headers=headers, params=querystring)
-    data: dict = response.json()
-    today: str = datetime.now().strftime('%Y-%m-%d')
-
-    expected_location_info: list = ['name', 'region', 'country', 'lat', 'lon', 'tz_id', 'localtime_epoch', 'localtime']
+def check_json_response(data: dict, response, expected_name: str, expected_region: str) -> None:
+    expected_location_info: list = ['name', 'region', 'country', 'lat', 'lon', 'tz_id', 'localtime_epoch',
+                                    'localtime']
     expected_current_info: list = ['last_updated_epoch', 'last_updated', 'temp_c', 'temp_f', 'is_day', 'condition',
                                    'wind_mph', 'wind_kph', 'wind_degree', 'wind_dir', 'pressure_mb', 'pressure_in',
                                    'precip_mm', 'precip_in', 'humidity', 'cloud', 'feelslike_c', 'feelslike_f',
                                    'vis_km', 'vis_miles', 'uv', 'gust_mph', 'gust_kph']
-
-    assert response.status_code == 200
-    assert all(loc_info in expected_location_info for loc_info in data['location'].keys())
-    assert all(cur_info in expected_current_info for cur_info in data['current'].keys())
-    assert data['location']['name'] == 'Recife'
-    assert data['location']['region'] == 'Pernambuco'
-    assert data['location']['country'] == 'Brazil'
-    assert today in data['location']['localtime']
-
-
-def test_valid_city_get_realtime_weather_api():
-    url: str = "https://weatherapi-com.p.rapidapi.com/current.json"
-
-    querystring: dict = {"q": "Sao Paulo"}
-
-    headers: dict = {
-        "X-RapidAPI-Key": 'e75696bd7cmsh2c6aeb979fa9e25p144730jsnf62c4bde84cd',
-        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
-    }
-
-    response = requests.get(url, headers=headers, params=querystring)
-    data: dict = response.json()
+    expected_country = 'Brazil'
+    expected_status = 200
+    location_keys: list = data['location'].keys()
+    current_keys: list = data['current'].keys()
     today: str = datetime.now().strftime('%Y-%m-%d')
 
-    expected_location_info: list = ['name', 'region', 'country', 'lat', 'lon', 'tz_id', 'localtime_epoch', 'localtime']
-    expected_current_info: list = ['last_updated_epoch', 'last_updated', 'temp_c', 'temp_f', 'is_day', 'condition',
-                                   'wind_mph', 'wind_kph', 'wind_degree', 'wind_dir', 'pressure_mb', 'pressure_in',
-                                   'precip_mm', 'precip_in', 'humidity', 'cloud', 'feelslike_c', 'feelslike_f',
-                                   'vis_km', 'vis_miles', 'uv', 'gust_mph', 'gust_kph']
-
-    assert response.status_code == 200
-    assert all(loc_info in expected_location_info for loc_info in data['location'].keys())
-    assert all(cur_info in expected_current_info for cur_info in data['current'].keys())
-    assert data['location']['name'] == 'Sao Paulo'
-    assert data['location']['region'] == 'Sao Paulo'
-    assert data['location']['country'] == 'Brazil'
+    assert response.status_code == expected_status
+    assert all(loc_info in expected_location_info for loc_info in location_keys)
+    assert all(cur_info in expected_current_info for cur_info in current_keys)
     assert today in data['location']['localtime']
+    assert data['location']['name'] == expected_name
+    assert data['location']['region'] == expected_region
+    assert data['location']['country'] == expected_country
 
 
-def test_valid_airport_get_realtime_weather_api():
-    url: str = "https://weatherapi-com.p.rapidapi.com/current.json"
-
-    querystring: dict = {"q": "CWB"}
-
-    headers: dict = {
+class TestWeatherApi:
+    URL: str = "https://weatherapi-com.p.rapidapi.com/current.json"
+    HEADERS: dict = {
         "X-RapidAPI-Key": 'e75696bd7cmsh2c6aeb979fa9e25p144730jsnf62c4bde84cd',
         "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
     }
 
-    response = requests.get(url, headers=headers, params=querystring)
-    data: dict = response.json()
-    today: str = datetime.now().strftime('%Y-%m-%d')
+    def test_valid_lat_lon_get_realtime_weather_api(self):
+        querystring: dict = {"q": "-8.063200,-34.871113"}
 
-    expected_location_info: list = ['name', 'region', 'country', 'lat', 'lon', 'tz_id', 'localtime_epoch', 'localtime']
-    expected_current_info: list = ['last_updated_epoch', 'last_updated', 'temp_c', 'temp_f', 'is_day', 'condition',
-                                   'wind_mph', 'wind_kph', 'wind_degree', 'wind_dir', 'pressure_mb', 'pressure_in',
-                                   'precip_mm', 'precip_in', 'humidity', 'cloud', 'feelslike_c', 'feelslike_f',
-                                   'vis_km', 'vis_miles', 'uv', 'gust_mph', 'gust_kph']
+        response = requests.get(url=self.URL, headers=self.HEADERS, params=querystring)
 
-    assert response.status_code == 200
-    assert all(loc_info in expected_location_info for loc_info in data['location'].keys())
-    assert all(cur_info in expected_current_info for cur_info in data['current'].keys())
-    assert data['location']['name'] == 'Curitiba Airport'
-    assert data['location']['region'] == 'Curitiba'
-    assert data['location']['country'] == 'Brazil'
-    assert today in data['location']['localtime']
+        check_json_response(
+            data=response.json(),
+            response=response,
+            expected_name='Recife',
+            expected_region='Pernambuco',
+        )
 
+    def test_valid_city_get_realtime_weather_api(self):
+        querystring: dict = {"q": "Sao Paulo"}
+        response = requests.get(url=self.URL, headers=self.HEADERS, params=querystring)
 
-def test_no_valid_parameter_get_realtime_weather_api():
-    url: str = "https://weatherapi-com.p.rapidapi.com/current.json"
+        check_json_response(
+            data=response.json(),
+            response=response,
+            expected_name='Sao Paulo',
+            expected_region='Sao Paulo',
+        )
 
-    querystring: dict = {"wrong_param": True}
+    def test_valid_airport_get_realtime_weather_api(self):
+        querystring: dict = {"q": "CWB"}
+        response = requests.get(url=self.URL, headers=self.HEADERS, params=querystring)
 
-    headers: dict = {
-        "X-RapidAPI-Key": 'e75696bd7cmsh2c6aeb979fa9e25p144730jsnf62c4bde84cd',
-        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
-    }
+        check_json_response(
+            data=response.json(),
+            response=response,
+            expected_name='Curitiba Airport',
+            expected_region='Curitiba',
+        )
 
-    response = requests.get(url, headers=headers, params=querystring)
-    data: dict = response.json()
+    def test_no_valid_parameter_get_realtime_weather_api(self):
+        querystring: dict = {"wrong_param": True}
+        response = requests.get(url=self.URL, headers=self.HEADERS, params=querystring)
+        data: dict = response.json()
 
-    assert response.status_code == 400
-    assert data['error']['code'] == 1003
-    assert data['error']['message'] == 'Parameter q is missing.'
+        assert response.status_code == 400
+        assert data['error']['code'] == 1003
+        assert data['error']['message'] == 'Parameter q is missing.'
 
+    def test_no_location_parameter_get_realtime_weather_api(self):
+        querystring: dict = {"q": 123456}
+        response = requests.get(url=self.URL, headers=self.HEADERS, params=querystring)
+        data: dict = response.json()
+        assert response.status_code == 400
+        assert data['error']['code'] == 1006
+        assert data['error']['message'] == 'No matching location found.'
 
-def test_no_location_parameter_get_realtime_weather_api():
-    url: str = "https://weatherapi-com.p.rapidapi.com/current.json"
+    def test_no_auth_get_realtime_weather_api(self):
+        querystring: dict = {"q": 123456}
 
-    querystring: dict = {"q": 123456}
+        headers: dict = {
+            "X-RapidAPI-Key": 'invalid_key',
+            "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+        }
 
-    headers: dict = {
-        "X-RapidAPI-Key": 'e75696bd7cmsh2c6aeb979fa9e25p144730jsnf62c4bde84cd',
-        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
-    }
-
-    response = requests.get(url, headers=headers, params=querystring)
-    data: dict = response.json()
-    assert response.status_code == 400
-    assert data['error']['code'] == 1006
-    assert data['error']['message'] == 'No matching location found.'
-
-
-def test_no_auth_get_realtime_weather_api():
-    url: str = "https://weatherapi-com.p.rapidapi.com/current.json"
-
-    querystring: dict = {"q": 123456}
-
-    headers: dict = {
-        "X-RapidAPI-Key": 'invalid_key',
-        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
-    }
-
-    response = requests.get(url, headers=headers, params=querystring)
-    data: dict = response.json()
-    assert response.status_code == 403
-    assert data['message'] == 'You are not subscribed to this API.'
+        response = requests.get(url=self.URL, headers=headers, params=querystring)
+        data: dict = response.json()
+        assert response.status_code == 403
+        assert data['message'] == 'You are not subscribed to this API.'
